dcc12696481e136bb69c4f92f2cb4d2a17339c87
Teo Sakel <teo.sakel@gmail.com>
2017-02-03 14:41:21 +0200

    Added calculabe LoB and LoD. Started working on fitting

--- R/calibrate_assay.R
+++ R/calibrate_assay.R
@@ -1,26 +1,68 @@
-calculateLoB <- function(x, conc = NULL, dist = c("Gaussian", "Robust", "Empirical"), a = 0.05) {
+calculateLoB <- function(x, conc = NULL, dist = c("Gaussian", "Robust", "Empirical"),
+                         a = 0.05) {
     dist <- match.arg(dist)
-    if (!is.null(conc)) x <- x[conc == 0]
-    # TODO: add option for student dist?
-    if (dist == "Gaussian") {
-        return(mean(x) + qnorm(1 - a) * sd(x))
-    } else if (dist == "Robust") {  # estimate mu, sigma with median and mad
-        # check here: http://stats.stackexchange.com/questions/67337/mad-in-relation-to-95-confidence
-        madx <- mad(x, constant = 1)
-        x <- x[(x - madx > 4) & (x + madx < 4)]  # trim outliers
-        return(median(x) + qnorm(1 - a) * mad(x))
-    } else if (dist == "Empirical") {
-        return(quantile(x, 1 - a))
+    if (a <= 0 || a >= 1) stop("a is a probability measure, it must be in the [0, 1] interval")
+    if (!is.null(conc)) {
+       if (! length(x) == length(conc)) stop("x and conc have different lengths")
+        zeros <- conc == 0
+        if (all(!zeros)) stop("No zero concentration was found")
+        x <- x[zeros]
     }
+
+    switch(dist,
+           Gaussian = mean(x) + qnorm(1 - a) * sd(x),
+           Robust = median(x) + qnorm(1 - a) * mad(x),
+           Empirical = quantile(x, 1 - a, type = 4))
 }
 
-calculateLoD <- function(x, conc, dist = c("Gaussian", "Robust", "Empirical"), a = 0.05) {
+calculateLoD <- function(x, conc, dist = c("Gaussian", "Robust", "Empirical"),
+                         a = 0.05, b = 0.05) {
     dist <- match.arg(dist)
+    if (b <= 0 || b >= 1) stop("b is a probability, it should be in the [0, 1] interval")
+    if (length(x) != length(conc)) stop("x and conc have different lengths")
+    LoB <- calculateLoB(x, conc, dist, a)
+    low_conc <- (x <= 4 * LoB) & (conc > 0)
+    x <- x[low_conc]
+    conc <- conc[low_conc]
+    if (dist == "Empirical") {
+        # assuming constant sd
+        Dsb <- aggregate(x ~ conc, FUN = function(y) quantile, probs = b, type = 4)
+        Dsb <- mean(Dsb$conc - Dsb$x)
+        return(LoB + Dsb)
+    }
+
+    df <- length(conc) - length(unique(conc))  # degrees of freedom
+    cb <- qnorm(1 - b) / (1 - 1/(4 * df))
+    if (dist == "Gaussian") {
+        SD <- sqrt(aggregate(x ~ conc, FUN = function(y) length(y) * var(y)) / length(x))
+    } else {
+        SD <- sqrt(aggregate(x ~ conc, FUN = function(y) length(y) * mad(y) ^ 2) / length(x))
+    }
+    return(LoB + cb * SD)
 }
 
-fit_PLR <- function(x, y, N = 4, useLog = TRUE) {
+fit_PLR <- function(dose, response, N = 5, useLog = TRUE, start = NULL, ...) {
     # fit Parametric logistic regression with N parameters, return the fitted object.
-    NULL
+    # check: http://www.sciencedirect.com/science/article/pii/S0163725802002231 and
+    # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1472692/
+    #
+    if (length(dose) != length(response)) stop("dose and response have different lengths.")
+    if (N != 4 && N != 5) stop("Only 5 and 4 parametric models are supported")
+    df <- data.frame(x = dose, y = response)
+    if (useLog) {
+        zeros <- df$x == 0
+        if (any(zeros)) warning("Zero doses are ignored for log-transformation")
+        df <- df[!zeros, , drop = FALSE]
+    }
+
+    if (is.null(start)) {
+        start <- c(A = min(df$y), B = 1, C = median(df$x), D = max(df$y), E = 1)
+    }
+
+    fmla <- switch(as.character(N),
+                   "4" = y ~ A + (D - A) / (1 + (x/C) ^ B),
+                   "5" = y ~ A + (D - A) / (1 + (x/C) ^ B) ^ E)
+    nls(fmla, data = df, start = start)
 }
 
 calibration_curve <- function(m) {
